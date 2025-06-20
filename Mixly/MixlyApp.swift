886fca634821fcbd42049a9250e3719555ca521f
Ross <rostislavpredein@gmail.com>
2025-06-19 23:49:07 -0300

    Initial Commit

---
+++ Mixly/MixlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixlyApp.swift
+//  Mixly
+//
+//  Created by Ross on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MixlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
